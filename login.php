e9410705ec30715534289190e5de91b5ce795d48
ArneBerckmans <arneberckmans@telenet.be>
2017-04-26 10:05:53 +0200

    login added

---
+++ login.php
@@ -0,0 +1,40 @@
+<?php
+
+
+
+
+?><!doctype HTML>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title></title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="stylesheet" type="text/css" href="css/reset.css">
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css">
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+
+    <script type="text/javascript" src="js/jquery-2.1.0.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+
+</head>
+<body>
+    <div class="container">
+        <form class="login" action="" method="post">
+            <legend>Login</legend>
+            <div>
+                <label for="email">Email</label>
+                <input value="" type="text" name="email" id="email" class="form-control">
+            </div>
+
+            <div>
+                <label type="text" name="username"></label>
+            </div>
+
+
+        </form>
+
+    </div>
+</body>
+</html>
