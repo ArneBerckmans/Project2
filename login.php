de5aaaf91383da7e9e67e95dc34b22788ce1c419
ArneBerckmans <arneberckmans@telenet.be>
2017-04-26 13:32:47 +0200

    login functie

--- login.php
+++ login.php
@@ -5,6 +5,25 @@ spl_autoload_register(function($class){
 });
 
 
+if (!empty($_POST)) {
+    if (!empty($_POST['userName']) && !empty($_POST['passWord'])) {
+
+        try {
+
+            $userName = $_POST['userName'];
+            $passWord = $_POST['passWord'];
+            $user = new User();
+            $user->setUserName($userName);
+            $user->setPassWord($passWord);
+
+            $user->login();
+
+        } catch (Exception $e) {
+            $error = $e->getMessage();
+        }
+    } else $error = "Vul uw gegevens in!";
+}
+
 ?><!doctype HTML>
 <html>
 
@@ -29,19 +48,24 @@ spl_autoload_register(function($class){
 
             <div>
                 <label for="username">Gebruikersnaam</label>
-                <input type="text" name="username" id="username" class="form-control">
+                <input type="text" name="userName" id="username" class="form-control" placeholder="Gebruikersnaam">
             </div>
 
             <div>
                <label for="password">Paswoord</label>
-                <input type="text" name="password" id="password" class="form-control">
+                <input type="password" name="passWord" id="password" class="form-control" placeholder="paswoord">
             </div>
 
             <button class="btn" type="submit" >Login</button>
             <p>OR</p>
             <a href="register.php">Registreer</a>
 
+            <?php
+            if (isset($error)): ?>
 
+                <div class="alert"><?php echo $error ?></div>
+
+            <?php endif; ?>
 
         </form>
 
